140f386f842847404b1f6d7b0140377bf531ec0b
Decendman Pohthmi <decendmanp@gmail.com>
2025-10-28 00:04:29 +0530

    Implement MaxElement class to find maximum value

---
+++ MaxElement.java
@@ -0,0 +1,10 @@
+public class MaxElement {
+    public static void main(String[] args) {
+        int[] arr = {10, 50, 20, 90, 70};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) max = arr[i];
+        }
+        System.out.println("Max element: " + max);
+    }
+}
